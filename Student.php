bb068064e7f80cf12daae03d50d25262f04925d0
Steven <sma52@my.bcit.ca>
2017-09-07 16:13:48 -0700

    Add Constructor

---
+++ Student.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Student
+ *
+ * @author steve
+ */
+class Student {
+    function __construct() {
+    $this->surname = '';
+    $this->first_name = '';
+    $this->emails = array();
+    $this->grades = array();
+}
+}

8237c449c9b5e37f58e61193903d24425b296e82
Steven <sma52@my.bcit.ca>
2017-09-07 16:15:22 -0700

    Add Some Mutator Methods

--- Student.php
+++ Student.php
@@ -13,9 +13,17 @@
  */
 class Student {
     function __construct() {
-    $this->surname = '';
-    $this->first_name = '';
-    $this->emails = array();
-    $this->grades = array();
-}
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
 }
